507bab155d5e677f08bed23f5045b59816b74121
Sanam Dhakal <sanam.dhakal@gmail.com>
2024-02-19 00:35:11 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-now everything seems okay
+ <?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?> 
